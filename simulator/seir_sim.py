29682ce435400fc0da6c807065cf4cfdaedb38a7
muhammadmada <45825969+muhammadmada@users.noreply.github.com>
2020-06-14 14:24:27 +0700

    Update seir_sim.py

--- simulator/seir_sim.py
+++ simulator/seir_sim.py
@@ -2,17 +2,44 @@ from scipy.integrate import odeint
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Plotting
+def modseirplot(t, S, Sq, E, Eq, I, H, R, L=None, D=None, R0=None, Alpha=None, CFR=None):
+  f, ax = plt.subplots(1,1, figsize=(10,4))
+  ax.plot(t, S, 'b', alpha=0.7, linewidth=2, label= 'Susceptible')
+  ax.plot(t, Sq, '', alpha=0.7, linewidth=2, label= 'Quarantined Susceptible')
+  ax.plot(t, E, 'y', alpha=0.7, linewidth=2, label= 'Exposed')
+  ax.plot(t, Eq, '', alpha=0.7, linewidth=2, label= 'Quarantined Exposed')
+  ax.plot(t, I, 'r', alpha=0.7, linewidth=2, label= 'Infected')
+  ax.plot(t, H, '', alpha=0.7, linewidth=2, label= 'Hospitalized')
+  ax.plot(t, R, 'g', alpha=0.7, linewidth=2, label= 'Recovered')
+  if D is not None:
+      ax.plot(t, D, 'k', alpha=0.7, linewidth=2, label='Dead')
+      ax.plot(t, S+Sq+E+Eq+I+H+R+D, 'c--', alpha=0.7, linewidth=2, label='Total')
+  else:
+       ax.plot(t, S+Sq+E+Eq+I+H+R+D, 'c--', alpha=0.7, linewidth=2, label='Total')
+    
+  ax.set_xlabel('Time (days)')
+  ax.yaxis.set_tick_params(length=0)
+  ax.xaxis.set_tick_params(length=0)
+  ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+  legend = ax.legend(borderpad=2.0)
+  legend.get_frame().set_alpha(0.5)
+  for spine in ('top','right', 'bottom', 'left'):
+      ax.spines[spine].set_visible(False)
+  if L is not None:
+     plt.title("Lockdown after {} days".format(L))
+  plt.show()
 #Model params
 c = float(3.6)
-beta = float(6.93*10^-11)
-deltaI = float(0.13)
-deltaQ = float(0.13)
-gammaI = float(0.003)
-gammaH = float(0.009)
+Beta = float(6.93*10^-11)
+DeltaI = float(0.13)
+DeltaQ = float(0.13)
+GammaI = float(0.003)
+GammaH = float(0.009)
 Q = float(9*10^-7)
-alpha = float(0.0001)
-theta = float(0.6)
-lamda = float(1/14)
+Alpha = float(0.0001)
+Theta = float(0.6)
+Lambda = float(1/14)
 T = float(40)
 t = float(0.1)
 NN = float(T/t)
@@ -24,19 +51,19 @@ Sq = int(8420)
 Eq = int(3000)
 H = Eq + I
 R = int(34)
-De = int(25)
+D = int(25)
 AA = [S, E , I , Sq, Eq, H, R]
 
 if ii == 1/NN:
-    sigma = float((1/7 - 1/3)/(1 + exp((ii*t-4)/ii*(t/0.2))+1/3)
-    dS = int(-(beta*c + c*q*(1-beta)*S*(I+theta+E)+ lamda*Sq)
-    dE =  int(beta*c*(1-q)*S*(I+theta*E)-sigma*E)
-    dI =  int(sigma*E-(deltaI+alpha+gammaI)*I)
-    dSq = int(1-beta)*c*q*S*(I+theta*E)-lamda*Sq)
-    dEq = int(beta*c*q*S*(I+theta*E)-deltaQ*Eq)
-    dH =  int(deltaI*I+deltaQ*Eq-(alpha+gama_H)*H)
-    dR =  int(gammaI*I+gama_H*H)
-    dDe = alpha*(I+H);
+    Sigma = float((1/7 - 1/3)/(1 + exp((ii*t-4)/ii*(t/0.2))+1/3)
+    dS = int(-(Beta*c + c*q*(1-Beta)*S*(I+Theta+E)+ Lambda*Sq)
+    dE =  int(Beta*c*(1-q)*S*(I+Theta*E)-Sigma*E)
+    dI =  int(Sigma*E-(deltaI+Alpha+GammaI)*I)
+    dSq = int(1-Beta)*c*q*S*(I+Theta*E)-Lambda*Sq)
+    dEq = int(Beta*c*q*S*(I+Theta*E)-DeltaQ*Eq)
+    dH =  int(deltaI*I+DeltaQ*Eq-(Alpha+GammaH)*H)
+    dR =  int(GammaI*I+GammaH*H)
+    dD = alpha*(I+H);
     #Euler integration algorithm
     S =S+dS*t
     E = E+dE*t
@@ -46,4 +73,4 @@ if ii == 1/NN:
     H = H+dH*t
     R = R+dR*t
     AA=[AA; S E I Sq Eq H R]
-    #Plotting algos below
+
